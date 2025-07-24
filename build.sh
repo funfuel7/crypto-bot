c8e81cc8c8d95ce7353ee07e46147ce5b233f215
funfuel7 <funfuel7@gmail.com>
2025-07-24 22:08:08 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
+# Set up isolated Python environment
+python -m venv venv
+source venv/bin/activate
+
+# Force upgrade core packages
+python -m pip install --upgrade --force-reinstall pip setuptools wheel
+
+# Install requirements with no cache
+pip install --no-cache-dir -r requirements.txt
